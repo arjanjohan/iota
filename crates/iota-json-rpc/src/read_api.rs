385ad5b58ed100aa9e1cdf286315a80c860c5fa1
Valerii Reutov <valeriy.reutov@gmail.com>
2025-04-08 10:33:04 +0300

    fix: ran cargo clippy

--- crates/iota-json-rpc/src/read_api.rs
+++ crates/iota-json-rpc/src/read_api.rs
@@ -1398,7 +1398,7 @@ fn get_value_from_move_struct(
                 }
             }
             _ => {
-                return Err(Error::UnexpectedError(format!(
+                Err(Error::UnexpectedError(format!(
                     "Unexpected move value type for field {}",
                     var_name
                 )))?;
