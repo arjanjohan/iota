53a68bfc17af60636cd1257c0d51220b0b003c1c
ilyavorobev <vorobyev.i.v@yandex.ru>
2025-04-26 14:17:47 +0200

    remove setting linearizer v2 flag

--- crates/starfish/core/src/linearizer.rs
+++ crates/starfish/core/src/linearizer.rs
@@ -547,9 +547,6 @@ mod tests {
         let num_authorities = 4;
         let (mut context, _keys) = Context::new_for_test(num_authorities);
         context.protocol_config.set_gc_depth_for_testing(0);
-        context
-            .protocol_config
-            .set_consensus_linearize_subdag_v2_for_testing(false);
 
         let context = Arc::new(context);
         let dag_state = Arc::new(RwLock::new(DagState::new(
